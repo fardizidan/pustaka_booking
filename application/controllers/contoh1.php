f6455ec45927d766ce9c7a6a1cd385e310437444
fardizidan <fardizidan2@gmail.com>
2022-10-23 10:27:27 +0700

    pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Fardi Zidan
+            Saya tinggal di daerah Ponorogo
+            olah raga yang saya sukai adalah
+            Badminton";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fardi</li>
+        <li>Nama Belakang : Zidan</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Sutowedono Dukuh.krajan Desa Nongkodono</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Game</li>
+        <li>Badmintoon</li>
+    </ul>
+</section>
